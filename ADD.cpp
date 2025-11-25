e5fd089dad30c5e703f888ba9588ffcf66b773bd
ordinaryturtle <ordinaryturtle01@gmail.com>
2025-11-25 19:16:53 +0530

    Update ADD.cpp

--- ADD.cpp
+++ ADD.cpp
@@ -8,22 +8,23 @@ int main() {
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    ptr = arr;  // Pointer points to the array
+    ptr = arr; 
 
     printf("Enter %d integers:\n", n);
     for (i = 0; i < n; i++) {
         printf("Element %d: ", i + 1);
-        scanf("%d", ptr + i);       // Store input using pointer
-        sum += *(ptr + i);          // Add value using pointer dereferencing
+        scanf("%d", ptr + i);       
+        sum += *(ptr + i);         
     }
 
     printf("\nArray elements are:\n");
     for (i = 0; i < n; i++) {
-        printf("%d ", *(ptr + i));  // Print values using pointer
+        printf("%d ", *(ptr + i));  
     }
 
     printf("\n\nSum of the elements = %d\n", sum);
 
     return 0;
 }
+
 
